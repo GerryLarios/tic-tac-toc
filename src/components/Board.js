f2caba3b1802b5f351a203765beaa55ec3ef2b8a
Gerardo Manuel Chavez Larios <gerardo.larios69@gmail.com>
2019-12-24 17:07:15 -0600

    [DEL] the state from board to pass it into the top level

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,54 +3,20 @@ import React, { Component } from "react";
 import Square from "./Square";
 import Row from "./Row";
 
-import { calculateWinner } from "../helpers/helpers";
-
 class Board extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-            xIsNext: true,
-            winner: null
-        };
-    }
-
-    handleClick(i) {
-        const squares = this.state.squares.slice();
-        if (this.state.winner || squares[i]) return;
-        squares[i] = this.nextPlayer();
-        this.setState({
-            squares,
-            xIsNext: !this.state.xIsNext,
-            winner: calculateWinner(squares)
-        });
-    }
 
     renderSquare(i) {
         return (
             <Square 
-                value={this.state.squares[i]} 
-                onClick={() => this.handleClick(i)}
+                value={this.props.squares[i]} 
+                onClick={() => this.props.onClick(i)}
             />
         );
     }
 
-    nextPlayer() {
-        return this.state.xIsNext ? 'X' : 'O';
-    }
-
-    updateStatus() {
-        return this.state.winner ? 
-            `Winner: ${this.state.winner}` : 
-            `Next player: ${this.nextPlayer()}`;
-    }
-
     render() {
-        const status = this.updateStatus();
         return(
             <div>
-                <div className="status">{status}</div>
                 <Row>
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
