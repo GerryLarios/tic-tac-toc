2a33c860ffda7c98904ceedf7717b572b5835b65
Gerardo Manuel Chavez Larios <gerardo.larios69@gmail.com>
2019-12-26 17:55:29 -0600

    Updates

---
+++ precache-manifest.59e338b8d63e1c776f857aa23efdce6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac83bbd9cb0b2a99fed8ec10ad89793e",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "445f910efa765ccb911f",
+    "url": "/tic-tac-toe/static/css/main.69bc152a.chunk.css"
+  },
+  {
+    "revision": "2553e5cbbee65392fea6",
+    "url": "/tic-tac-toe/static/js/2.79e0bc18.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tic-tac-toe/static/js/2.79e0bc18.chunk.js.LICENSE"
+  },
+  {
+    "revision": "445f910efa765ccb911f",
+    "url": "/tic-tac-toe/static/js/main.136709d5.chunk.js"
+  },
+  {
+    "revision": "14c00c4b9a734bdeb7a0",
+    "url": "/tic-tac-toe/static/js/runtime-main.1fec3c3c.js"
+  }
+]);
